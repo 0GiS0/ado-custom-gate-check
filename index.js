cd23048f741ac99b63f7ef1c36f2fb10013b36e6
0GiS0 <giselatb@outlook.com>
2023-11-02 09:25:26 +0000

    add some dummy logic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const express = require('express');  
+const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,7 +9,30 @@ app.use(express.json());
 
 app.post('/hook', async (req, res) => {
 
+
+    //Check headers
+    console.log(req.headers);
+
+    let jobId = req.headers['jobid'],
+        taskId = req.headers['taskinstanceid'];
+
     console.log(`Received webhook: ${JSON.stringify(req.body)}`);
+
+    let environment = req.body.environment;
+
+    if (environment == "dev") {
+
+        // Send Approval
+        const approval = {
+            "name": "TaskCompleted",
+            "taskId": taskId,
+            "jobId": jobId,
+            "result": "succeeded"
+        };
+    }
+    else if (environment == "prod") {
+        // Send Rejection
+    }
 
     res.status(200).send('OK');
 });
